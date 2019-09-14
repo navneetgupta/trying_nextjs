091de8169b9401a61b6f10008cdf9a6e48f13bcf
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-14 15:19:02 +0400

    Lecture : 419. Understanding Server Side Rendering

---
+++ pages/auth/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthIndexPage = () => (
+  <div>My AuthIndexPage index page rendered by next.js</div>
+);
+
+export default AuthIndexPage;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const IndexPage = () => <div>My First index page rendered by next.js;
+
+export default IndexPage;
