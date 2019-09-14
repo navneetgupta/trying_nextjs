cf7a2588b83e745d526560e33bc8a820315b200c
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-14 15:41:51 +0400

    Lecture 422. Next.js & Components & Pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import User from "../../components/User";
 
 const AuthIndexPage = () => (
   <div>
     <h2>My AuthIndexPage index page rendered by next.js</h2>
+    <User name="Navneet Gupta" age={28} />
   </div>
 );
 
