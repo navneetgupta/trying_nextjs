6dd94e21c3bc6be68c36f40af7b6f30a038df630
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-14 15:48:44 +0400

    Lecture 423. Styling our App in Next.js

--- components/User.js
+++ components/User.js
@@ -5,6 +5,14 @@ const User = props => {
     <div>
       <h1>{props.name}</h1>
       <p>Age: {props.age}</p>
+      <style jsx>{`
+        div {
+          text-align: center;
+          border: 1px solid #eee;
+          box-shadow: 0 2px 3px #ccc;
+          padding: 20px;
+        }
+      `}</style>
     </div>
   );
 };
