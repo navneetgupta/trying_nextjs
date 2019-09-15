2956eca5f28a69e6b9311ce2280fdee2d457f1b3
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-09-15 09:29:15 +0400

    Lecture 426. Class based Componenet in nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import Link from "next/link";
 import Router from "next/router";
 
-const IndexPage = () => (
-  <div>
-    <h2>My First index page rendered by next.js</h2>
-    <p>
-      Go to{" "}
-      <Link href="/auth">
-        <a>Auth</a>
-      </Link>
-    </p>
-    <button onClick={() => Router.push("/auth")}>Go to Auth</button>
-  </div>
-);
+class IndexPage extends Component {
+  static getInitialProps(context) {
+    //async method
+    console.log(context); // Server side lifecycle hook
+    // can be used to fetch data from another server/DB e tc
+    return {};
+  }
+  render() {
+    return (
+      <div>
+        <h2>My First index page rendered by next.js</h2>
+        <p>
+          Go to{" "}
+          <Link href="/auth">
+            <a>Auth</a>
+          </Link>
+        </p>
+        <button onClick={() => Router.push("/auth")}>Go to Auth</button>
+      </div>
+    );
+  }
+}
 
 export default IndexPage;
